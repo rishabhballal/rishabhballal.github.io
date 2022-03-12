079d2f2eb15eb16fa41c21d789fb95ac847b74b8
Rishabh Ballal <rishabhballal@gmail.com>
2022-03-12 22:07:36 +0100

    add orientation

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function geodesic(z1) {
   const r = 100; // dynamic
   let phi = 0;
-  const theta = Math.PI/4;
   let x = [[], [], []];
+  const theta = 0.4*Math.PI;
   while (phi < 2*Math.PI) {
     x[0].push(r*Math.cos(phi));
-    x[1].push(r*Math.sin(phi));
-    x[2].push(z1);
-    phi += 0.02;
+    x[1].push(r*Math.sin(phi)*Math.cos(theta) - z1*Math.sin(theta));
+    x[2].push(r*Math.sin(phi)*Math.sin(theta) + z1*Math.cos(theta));
+    phi += 0.01;
   }
   return x;
 }
@@ -17,8 +17,8 @@ function projection(x) {
   let y = [[], []];
   i = 0;
   while (i < x[0].length) {
-    y[0].push(o[0] + x[0][i]/(1-x[2][i]));
-    y[1].push(o[1] + x[1][i]/(1-x[2][i]));
+    y[0].push(o[0] + 300*x[0][i]/(120-x[2][i]));
+    y[1].push(o[1]/2 + 200*x[1][i]/(120-x[2][i]));
     i++;
   };
   return y;
@@ -30,8 +30,8 @@ function plot(c, x) {
     c.moveTo(x[0][i-1], x[1][i-1]);
     c.lineTo(x[0][i], x[1][i]);
   }
-  c.lineWidth = 0.4;
-  c.strokeStyle = '#444';
+  c.lineWidth = 0.2;
+  c.strokeStyle = '#777';
   c.stroke();
 }
 
@@ -40,7 +40,7 @@ canvas.width = window.innerWidth;
 canvas.height = 0.8*window.innerHeight;
 if (canvas.getContext('2d')) {
   const c = canvas.getContext('2d');
-  [0.4, 0.5, 0.6].forEach(z => plot(c, projection(geodesic(z))));
+  [15, 5, -5, -15].forEach(z => plot(c, projection(geodesic(z))));
 
 } else {
 
